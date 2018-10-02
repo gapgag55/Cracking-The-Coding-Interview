37b6be2f55f1fe7cd15ee36bd07b2b51a9ba27bc
gapgag55 <imkopkap@gmail.com>
2018-10-03 00:14:36 +0700

    Update

--- chapter2/2.1/2.1-remove-dups.c
+++ chapter2/2.1/2.1-remove-dups.c
@@ -3,6 +3,6 @@
 // Description: Write code to remove duplicates from an unsorted linked list. How would you solve problem if a temporary buffer is not allowed?
 
 int main() {
-  printf("Hello C programming");
+  printf("Hello World");
   return 0;
 }
